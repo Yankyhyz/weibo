584dc003a8c3420cb87769338f20b4ab05e1bb27
yanky <yanky123@example.com>
2021-09-12 15:22:45 +0800

    管理员可删用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,18 +12,18 @@ class UsersController extends Controller
         $this->middleware('auth',[
             'except'=>['show','create','store','index']
         ]);
-        
+
         $this->middleware('guest',[
             'only'=>['create']
         ]);
     }
-    
+
     public function index(){
         // $users=User::all();
         $users=User::paginate(6);
         return view('users.index',compact('users'));
     }
-    
+
     public function create(){
         return view('users.create');
     }
@@ -72,6 +72,13 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user);
+    }
+
+    public function destroy(User $user){
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success','成功删除用户！');
+        return back();
     }
 
 }
