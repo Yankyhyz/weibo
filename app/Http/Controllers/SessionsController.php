75f2adf0df5656603cda1888fae7fbac443f555f
yanky <yanky123@example.com>
2021-09-13 23:33:30 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,9 +24,17 @@ class SessionsController extends Controller
 
        if(Auth::attempt($credentials, $request->has('remember'))){
           //登录成功后的相关操作
-          session()->flash('success','欢迎回来！');
-          $fallback=route('users.show',Auth::user());
-          return redirect()->intended($fallback);
+
+          if(Auth::user()->activated){
+              session()->flash('success','欢迎回来！');
+              $fallback=route('users.show',Auth::user());
+              return redirect()->intended($fallback);
+          }else{
+              Auth::logout();
+              session()->flash('warnimg','你的账号未激活，请检查邮箱中的注册邮件进行激活！');
+              return redirect('/');
+          }
+
        }else{
            //登录失败后的相关操作
            session()->flash('danger','很抱歉，您的邮箱和密码不匹配！');
@@ -35,7 +43,7 @@ class SessionsController extends Controller
        }
 
     }
-    
+
     public function destroy(){
         Auth::logout();
         session()->flash('success','您已经成功退出！');
